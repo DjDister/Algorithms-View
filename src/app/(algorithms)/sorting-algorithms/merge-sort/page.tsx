ace163984d4c01acd8abb8129065b7bb60f95ffc
DjFIREED <porebskifilip@wp.pl>
2024-02-18 20:26:07 +0100

    first merge sort attempt

---
+++ src/app/(algorithms)/sorting-algorithms/merge-sort/page.tsx
@@ -0,0 +1,10 @@
+import MergeVisulization from "@/components/MergeVisulization";
+
+export default function Page() {
+  return (
+    <div>
+      <h1>Merge Sort</h1>
+      <MergeVisulization />
+    </div>
+  );
+}
