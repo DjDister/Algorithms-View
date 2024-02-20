ba28867abee272464b0071e7a810658c2777e9da
DjFIREED <porebskifilip@wp.pl>
2024-02-20 09:44:51 +0100

    wip Add arrow-down SVG and highlight comparing elements in MergeVisualization

--- src/components/ChartVisualization/ChartVisualization.tsx
+++ src/components/ChartVisualization/ChartVisualization.tsx
@@ -2,16 +2,48 @@
 
 import React, { useEffect } from "react";
 import styles from "./ChartVisualization.module.css";
+import Image from "next/image";
+export default function ChartVisualization({
+  array,
+  highliteItems,
+  highliteOne,
+  highliteTwo,
+}: {
+  array: number[];
+  highliteItems: number[][];
+  highliteOne: number | null;
+  highliteTwo: number | null;
+}) {
+  const firstHighlite = highliteItems[0];
+  const secondHighlite = highliteItems[1];
+  const leftColor = "red";
+  const rightColor = "blue";
 
-export default function ChartVisualization({ array }: { array: number[] }) {
+  console.log(`highliting: `, highliteOne, highliteTwo);
   return (
     <div className={styles.chartContainer}>
       {array.map((value, index) => (
-        <div
-          key={index}
-          className={styles.chartBar}
-          style={{ height: `${value * 10}px` }}
-        />
+        <div key={index}>
+          {(highliteOne === value || highliteTwo === value) && (
+            <Image
+              src="/svgs/ArrowDown.svg"
+              alt="arrow-down"
+              width={20}
+              height={20}
+            />
+          )}
+          <div
+            className={styles.chartBar}
+            style={{
+              height: `${value * 10}px`,
+              backgroundColor: firstHighlite.includes(value)
+                ? leftColor
+                : secondHighlite.includes(value)
+                ? rightColor
+                : "grey",
+            }}
+          />
+        </div>
       ))}
     </div>
   );
--- src/components/MergeVisulization.tsx
+++ src/components/MergeVisulization.tsx
@@ -5,9 +5,19 @@ import ChartVisualization from "./ChartVisualization/ChartVisualization";
 import { createRandomArray, shuffle } from "../utils/arrays-utils";
 
 const initialArray = createRandomArray(20);
-
 export default function MergeVisualization() {
   const [chartData, setChartData] = useState(initialArray);
+  const [leftCompared, setLeftCompared] = useState<number | null>(null);
+  const [rightCompared, setRightCompared] = useState<number | null>(null);
+  const [leftComparing, setLeftComparing] = useState<number[]>([]);
+  const [rightComparing, setRightComparing] = useState<number[]>([]);
+
+  const removeItemsAndSetThemSorted = (sorted: number[]) => {
+    setChartData((prev) => {
+      const newChartData = prev.filter((item) => !sorted.includes(item));
+      return [...sorted, ...newChartData];
+    });
+  };
 
   const mergeSort = async (array: number[]): Promise<number[]> => {
     if (array.length <= 1) return array;
@@ -15,16 +25,26 @@ export default function MergeVisualization() {
     const mid = Math.floor(array.length / 2);
     const left: number[] = await mergeSort(array.slice(0, mid));
     const right: number[] = await mergeSort(array.slice(mid));
-
-    return merge(left, right);
+    const mergeResult = await merge(left, right);
+    // removeItemsAndSetThemSorted(mergeResult);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    console.log(`mergeResult: `, mergeResult);
+    return mergeResult;
   };
 
   const merge = async (left: number[], right: number[]) => {
     const result = [];
     let i = 0,
       j = 0;
+    console.log(`left: `, left);
+    console.log(`right: `, right);
+    setLeftComparing(left);
+    setRightComparing(right);
 
     while (i < left.length && j < right.length) {
+      console.log(`comparing left[i]: ${left[i]} and right[j]: ${right[j]}`);
+      setLeftCompared(left[i]);
+      setRightCompared(right[j]);
       if (left[i] < right[j]) {
         result.push(left[i]);
         i++;
@@ -32,18 +52,28 @@ export default function MergeVisualization() {
         result.push(right[j]);
         j++;
       }
-      await new Promise((resolve) => setTimeout(resolve, 100));
-      setChartData([...result, ...left.slice(i), ...right.slice(j)]);
+      console.log(`result with pushed`, result);
+      await new Promise((resolve) => setTimeout(resolve, 1000));
+      // setChartData([...result, ...left.slice(i), ...right.slice(j)]);
+      removeItemsAndSetThemSorted(
+        result.concat(left.slice(i)).concat(right.slice(j))
+      );
     }
 
     return result.concat(left.slice(i)).concat(right.slice(j));
   };
 
   const handleRandomize = async () => {
-    setChartData(() => shuffle(chartData));
+    setChartData(() => {
+      const shuffled = shuffle(chartData);
+      console.log(`chartData: `, shuffled);
+      return shuffled;
+    });
   };
 
   const handleSort = async () => {
+    console.log(`chartData: `, chartData);
+
     const sorted = await mergeSort(chartData);
   };
 
@@ -51,7 +81,12 @@ export default function MergeVisualization() {
     <div>
       <button onClick={handleRandomize}>Shuffle</button>
       <button onClick={handleSort}>Sort</button>
-      <ChartVisualization array={chartData} />
+      <ChartVisualization
+        array={chartData}
+        highliteItems={[leftComparing, rightComparing]}
+        highliteOne={leftCompared}
+        highliteTwo={rightCompared}
+      />
     </div>
   );
 }
